40dc139fadeabdcabd8c810f17b1a0576ec522fd
Piotr Komosa <komosa.piotr@gmail.com>
2019-11-14 20:01:06 +0100

    commit5

--- src/zad2/DoubleList.java
+++ src/zad2/DoubleList.java
@@ -7,10 +7,8 @@ import java.util.Scanner;
 public class DoubleList {
 
     public static void main(String[] args) {
-
         showNumbersAndSum(printNumber());
     }
-
     static List<Integer> printNumber() {
         List<Integer> numbers = new ArrayList<>();
         System.out.println("Podaj liczby:");
@@ -24,20 +22,16 @@ public class DoubleList {
         System.out.println("Liczby musza byc większe od 0");
         return numbers;
     }
-
     private static void showNumbersAndSum(List<Integer> numbers) {
-
         for (int i = numbers.size() - 1; i >= 0; i--) {
             System.out.println(numbers.get(i));
-
         }
         sum(numbers);
         System.out.println();
     }
-
     private static void sum(List<Integer> numbers) {
         int sum = 0;
-        System.out.println("wdafia");
+        System.out.println("Suma liczb to:");
         for (int i = 0; i < numbers.size(); i++) {
             if (i == (numbers.size() - 1)) {
                 System.out.print(numbers.get(i));
@@ -53,7 +47,6 @@ public class DoubleList {
         int min = numbers.get(0);
         int max = 0;
         for (int i : numbers) {
-
             if (max < i) {
                 max = i;
             }
@@ -63,35 +56,9 @@ public class DoubleList {
                 min = i;
             }
         }
-        System.out.println("Max =" + max);
-        System.out.println("Min =" + min);
+        System.out.println("Wartość Max to:" + max);
+        System.out.println("Wartość Min to:" + min);
     }
-
 }
-
-
-//    private static void sum(List<Double> numbers) {
-//        double sum = 0;
-//        for (int i = 0; i < numbers.size(); i++) {
-//            sum += numbers.get(i);
-//        }
-//    }
-
-
-//    private static void add(int numberOfLines) {
-//        System.out.println("podaj liczby:");
-//        Scanner scanner = new Scanner(System.in);
-//        List<Double> numbers = new ArrayList<>();
-//       // double userInput;
-//        double sum=0;
-//        while(scanner.nextDouble()>0){
-//            double userInput = scanner.nextDouble();
-//            sum += userInput;
-//
-//        }
-//        System.out.println("Suma to" + sum);
-//
-//    }
-
 
 
