6a201322277c8c5570a52164fc943a319960db2a
Piotr Komosa <komosa.piotr@gmail.com>
2019-11-14 17:57:10 +0100

    commit2

--- src/zad2/DoubleList.java
+++ src/zad2/DoubleList.java
@@ -11,7 +11,9 @@ public class DoubleList {
 
         showNumbersAndSum(printNumber());
 
+
     }
+
     static List<Integer> printNumber() {
         List<Integer> numbers = new ArrayList<>();
         System.out.println("Podaj liczby:");
@@ -19,28 +21,59 @@ public class DoubleList {
         Scanner scanner = new Scanner(System.in);
         do {
             input = scanner.nextInt();
-            numbers.add(input);
+            if (input >= 0)
+                numbers.add(input);
         } while (input >= 0);
         System.out.println("Liczby musza byc większe od 0");
         return numbers;
     }
-    private static void  showNumbersAndSum(List<Integer> numbers) {
 
+    private static void showNumbersAndSum(List<Integer> numbers) {
 
-        for (int i = numbers.size()-1; i >= 0 ; i--) {
+        for (int i = numbers.size() - 1; i >= 0; i--) {
             System.out.println(numbers.get(i));
 
         }
-
-
-        }
+        sum(numbers);
+        System.out.println();
 
     }
 
-       
+    private static void sum(List<Integer> numbers) {
+        int sum = 0;
+        System.out.println("wdafia");
+        for (int i = 0; i < numbers.size(); i++) {
+            if (i == numbers.size()) {
+                System.out.print(numbers.get(i));
+            } else {
+                System.out.print(numbers.get(i) + "+");
+            }
+            sum += numbers.get(i);
+        }
+        System.out.println("=" + sum);
+        minMax(numbers);
+    }
+
+    private static void minMax(List<Integer> numbers) {
+        int min = numbers.get(0);
+        int max = 0;
+        for (int i : numbers) {
+
+            if (max < i) {
+                max = i;
+            }
+        }
+        for (int i : numbers) {
+            if (min > i) {
+                min = i;
+            }
+        }
+        System.out.println("Max =" + max);
+        System.out.println("Min =" + min);
+    }
 
 
-
+}
 
 
 //    private static void sum(List<Double> numbers) {
